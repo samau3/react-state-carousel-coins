938ce5f356a7c69259382ed50df2a0729671d299
samau3 <samau415@gmail.com>
2021-10-12 15:47:01 -0700

    added test for left arrow functionality and fixed left arrow bug

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,6 +2,22 @@ import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 import TEST_IMAGES from "./_testCommon.js";
 
+test("renders without crashing", function () {
+  render(<Carousel
+    photos={TEST_IMAGES}
+    title="images for testing"
+  />);
+});
+
+test("matches snapshot", function () {
+  const { container } = render(<Carousel
+    photos={TEST_IMAGES}
+    title="images for testing"
+  />);
+  expect(container).toMatchSnapshot();
+});
+
+
 it("works when you click on the right arrow", function() {
   const { container } = render(
     <Carousel
@@ -30,17 +46,44 @@ it("works when you click on the right arrow", function() {
   ).toBeInTheDocument();
 });
 
-test("renders without crashing", function () {
-  render(<Carousel
-    photos={TEST_IMAGES}
-    title="images for testing"
-  />);
+
+it("works when you click on the left arrow", function() {
+  const { container, debug } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  // debug(container)
+
+  // app render starts at image 1, so need to move forward one image before
+  // testing if leftArrow behaves appropriately
+  const rightArrow = container.querySelector(".fa-chevron-circle-right");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+
+
+  // move back in the carousel
+  const leftArrow = container.querySelector(".fa-chevron-circle-left");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
 });
 
-test("matches snapshot", function () {
-  const { container } = render(<Carousel
-    photos={TEST_IMAGES}
-    title="images for testing"
-  />);
-  expect(container).toMatchSnapshot();
-});
+
+
+
+
